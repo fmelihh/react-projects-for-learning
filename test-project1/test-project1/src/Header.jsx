aaa5438ac390033a5009c84a88b9d096de6489f1
Furkan Melih Ercan <99879453+fmelihh@users.noreply.github.com>
2024-04-28 17:07:44 +0300

    feat: 2 projects were developed.

---
+++ test-project1/test-project1/src/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header() {
+    return (
+        <header>
+            <h1>Your Website !</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Content</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+    
+}
+
+export default Header;
